d382b396b0d893dbba177bbb72c50fec06a3a1eb
Ravikant Sharma <ravikant.sharma@worldpay.com>
2016-11-09 18:41:17 +0530

    ok

--- src/main/java/com/test/rest/HelloWorldService.java
+++ src/main/java/com/test/rest/HelloWorldService.java
@@ -5,6 +5,7 @@ package com.test.rest;
 
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
@@ -17,13 +18,24 @@ import javax.ws.rs.core.Response;
 public class HelloWorldService {
 
 	@GET
-	@Path("/{param}")
+	@Path("say/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
 
 		String output = "Jersey say : " + msg;
 
 		return Response.status(200).entity(output).build();
+		
+	}
+	
+	@GET
+	@POST
+	@Path("/")
+	public Response info() {
 
+		String output = "Hello from jersey !!!!!!!!!!!!!";
+
+		return Response.status(200).entity(output).build();
+		
 	}
 
 }
