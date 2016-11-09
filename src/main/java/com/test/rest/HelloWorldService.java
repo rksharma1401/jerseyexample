749d7bed20e9596480c591308f21d43eaa0e85cb
Ravikant Sharma <ravikant.sharma@worldpay.com>
2016-11-09 18:37:42 +0530

    added service

---
+++ src/main/java/com/test/rest/HelloWorldService.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.test.rest;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+ 
+/**
+ * @author ravikant.sharma
+ * Nov 9, 2016
+ */
+@Path("hello")
+public class HelloWorldService {
+
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+
+		String output = "Jersey say : " + msg;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+}
