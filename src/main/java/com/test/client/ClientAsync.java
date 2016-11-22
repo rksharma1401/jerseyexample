e2c602926d7696207a06b769be350c5578a80ca2
Ravikant Sharma <ravikant.sharma@worldpay.com>
2016-11-22 19:00:02 +0530

    client

---
+++ src/main/java/com/test/client/ClientAsync.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package com.test.client;
+
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.InvocationCallback;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author ravikant.sharma Nov 18, 2016
+ */
+public class ClientAsync {
+
+	
+	static Future<Response> responseFuture = null;
+	static long betweenTime = 0;
+
+	public static void main(String[] args) throws Exception {
+		Client client = ClientBuilder.newClient();
+		// System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
+
+		/*
+		 * Response response = client.target(
+		 * "http://jerseyexample-ravikant.rhcloud.com/rest/jws/resource").
+		 * request().get(); System.out.println(response.getEntity());
+		 */
+		final long startTime = new Date().getTime();
+
+		responseFuture = client.target("http://jerseyexample-ravikant.rhcloud.com/rest/jws/resource").request().async()
+				.get(new InvocationCallback<Response>() {
+
+					@Override
+					public void completed(Response response) {
+						System.out.println("Response status code " + response.getStatus() + " received." + "\nMessage :"
+								+ response.readEntity(String.class));
+						betweenTime = new Date().getTime();
+						System.out.println("Time Taken " + (betweenTime - startTime) + TimeUnit.MILLISECONDS);
+					}
+
+					@Override
+					public void failed(Throwable throwable) {
+						System.out.println("Invocation failed due to" + throwable.getMessage());
+						// throwable.printStackTrace();
+						System.out.println("is isCancelled " + responseFuture.isCancelled());
+					}
+
+				});
+
+		// System.out.println(responseFuture.cancel(true));
+
+		Future<String> future1 = client.target("http://jerseyexample-ravikant.rhcloud.com/rest/jws/resource").request()
+				.async().get(String.class);
+		try {
+
+			System.err.println("future1 Response" + future1.get(6000, TimeUnit.MILLISECONDS));
+		} catch (Exception e) { 
+			System.err.println(e.getMessage());
+			future1.cancel(true); // this method will stop
+														// running underlying
+														// task
+		}
+
+		System.out.println("Main");
+	}
+
+}
