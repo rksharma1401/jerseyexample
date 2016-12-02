99a1d7c1a4da97f44da50530fd4271fddf10f9b4
Ravikant Sharma <ravikant.sharma@worldpay.com>
2016-12-02 15:41:05 +0530

    UserVo

---
+++ src/main/java/com/test/vo/User.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.test.vo;
+
+/**
+ * @author ravikant.sharma
+ * 02-Dec-2016
+ */
+public class User {
+
+	private String name; 
+	private String company;	
+	private String post;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getPost() {
+		return post;
+	}
+	public void setPost(String post) {
+		this.post = post;
+	}
+	
+}
