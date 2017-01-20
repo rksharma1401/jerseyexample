8b522f54a11a212ec0d55726ae6d3d447c782e20
Ravikant Sharma <rksharma1401@hotmail.com>
2017-01-20 13:59:39 +0530

    sysout added

--- src/main/java/com/study/rest/ReqLoggingFilter.java
+++ src/main/java/com/study/rest/ReqLoggingFilter.java
@@ -4,8 +4,6 @@
 package com.study.rest;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
@@ -16,12 +14,11 @@ import javax.ws.rs.client.ClientRequestFilter;
  * 20-Jan-2017
  */
 public class ReqLoggingFilter implements ClientRequestFilter {
-    private static final Logger LOG = Logger.getLogger(ReqLoggingFilter.class.getName());
-
+     
     @Override
     public void filter(ClientRequestContext requestContext) throws IOException {
     	try{
-        LOG.log(Level.ALL, requestContext.toString());
+    			System.out.println(requestContext.toString());
     	}catch(Exception e){
     		e.printStackTrace();
     	}
