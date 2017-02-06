52b03fd49e9a1e362f6c45d0e98d314d94752597
Ravikant Sharma <rksharma1401@hotmail.com>
2017-02-06 14:09:38 +0530

    Added MultiPartFeature

--- src/main/java/com/study/config/ApplicationConfig.java
+++ src/main/java/com/study/config/ApplicationConfig.java
@@ -5,7 +5,9 @@ package com.study.config;
  
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -13,7 +15,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 import org.glassfish.jersey.CommonProperties;
-import org.glassfish.jersey.server.ServerProperties;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 @ApplicationPath("/rest")
 public class ApplicationConfig extends Application {
@@ -28,7 +30,18 @@ public class ApplicationConfig extends Application {
         System.out.println("getProperties:-> CommonProperties.OUTBOUND_CONTENT_LENGTH_BUFFER_SERVER :" + CommonProperties.getValue(properties,CommonProperties.OUTBOUND_CONTENT_LENGTH_BUFFER,String.class));
         return properties;
     }
-      
+    
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> resources = new HashSet<Class<?>>();
+
+        // Add your resources.
+//        resources.add(JerseyWebService.class);
+
+        // Add additional features such as support for Multipart.
+        resources.add(MultiPartFeature.class);
+
+        return resources;
+    }
     @PreDestroy
     public void preDestroy() {
       System.out.println("************************** @PreDestroy **************************************");
