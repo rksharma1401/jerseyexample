98a93b739a534a7ac2a2e579bbd973c76eacb0eb
Ravikant Sharma <rksharma1401@hotmail.com>
2017-05-03 13:11:46 +0530

    add hotdeploy and try catch

--- src/main/java/com/study/db/HibernateConnection.java
+++ src/main/java/com/study/db/HibernateConnection.java
@@ -3,24 +3,27 @@ package com.study.db;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
- 
 
 public class HibernateConnection {
-	static SessionFactory sessionFactory =null;
-	static{
+	static SessionFactory sessionFactory = null;
+	static {
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure("hibernate.cfg.xml");
+			System.out.println("Hibernate Configuration created successfully");
+			sessionFactory = configuration.buildSessionFactory();
+			System.out.println("SessionFactory created successfully");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		Configuration configuration = new Configuration();
-    	configuration.configure("hibernate.cfg.xml");
-    	System.out.println("Hibernate Configuration created successfully");
-    	sessionFactory = configuration.buildSessionFactory();
-    	System.out.println("SessionFactory created successfully");
-    	 
-	
 	}
-	public HibernateConnection(){}
-	
-	public static Session getSession(){
+
+	public HibernateConnection() {
+	}
+
+	public static Session getSession() {
 		return sessionFactory.getCurrentSession();
 	}
-	
+
 }
